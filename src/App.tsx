883755d2ce1d8a9a30c3a7dd5bf82d080eb7429f
zein <173869188+gitzein@users.noreply.github.com>
2025-11-11 10:27:08 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="bg-neutral-950 h-screen flex items-center justify-center">
+        <h1 className="font-bold tracking-wide text-neutral-100 text-4xl">
+          Wordle Game
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
